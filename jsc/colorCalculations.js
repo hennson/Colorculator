86798e231482c9c3b39221628039b8cd370cb63b
Henning <hf@usw-usf.de>
2011-10-30 10:18:03 +0100

    Initial version of the Colorculator (tutorial) WebApp

---
+++ jsc/colorCalculations.js
@@ -0,0 +1,105 @@
+/*
+* Version: 00.01
+* Date: 29.10.2011
+* By: usw-usf.de
+*/
+
+/* hex --> rgb
+* works with color shorthands and the full representation
+* (#fff or #ffffff)
+*/
+function hexToRgb(hex) {
+
+// nothing to do
+if (hex.length < 3) return null;
+    
+    var temp,hxv;
+    hxv = temp = hex;
+    
+    if (hxv.length == 3) {
+
+        hxv = "";
+        temp = /^([a-f0-9])([a-f0-9])([a-f0-9])$/i .exec(temp).slice(1);
+
+        for (var i = 0; i < 3; i++) {
+            var col = temp[i];
+            // mak a 2-tuple out of it
+            hxv += col + col;
+        }
+    }
+
+    if (hxv.length == 6) {
+        var triplets = /^([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i .exec(hxv).slice(1);
+
+        // prepare for calculation
+        hxv = "0x" + triplets.join("");
+        return {
+            R: (hxv & 0xff0000) >> 16,
+            G: (hxv & 0x00ff00) >> 8,
+            B: (hxv & 0x0000ff)
+        };
+    }
+    
+    return null;
+}
+
+/* rgb --> hsl
+* according to algorithm from http://en.wikipedia.org/wiki/HSL_color_space.
+* result converted to degree, percent, percent 
+*/
+function rgbToHsl(r, g, b){
+   
+    r /= 255,
+    g /= 255, 
+    b /= 255;
+    
+    var max = Math.max(r, g, b), 
+        min = Math.min(r, g, b);
+    var h = 0,
+        s = 0,
+        l = (max + min) / 2;
+
+	if(max == min){
+		// gray
+        h = s = 0; 
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return {    
+    	H: Math.floor(h * 360),
+    	S: Math.floor(s * 100),
+    	L: Math.ceil(l * 100)
+    };
+}
+
+/* set font color for dark or light background
+ * based on a C# algorithm from Gacek
+ * http://stackoverflow.com/questions/1855884/determine-font-color-based-on-background-color
+ */
+function setFontColor(color)
+{
+            var d = 0;
+            var a = 1 - ( 0.299 * color.R + 0.587 * color.G + 0.114 * color.B)/255;
+
+            if (a < 0.5) {
+            	// bright bg --> black font
+            	d = 60; 
+            } else {
+            	// dark bg --> white font
+            	d = 255;	
+            }
+                 
+           	return {    
+		    	R: Math.floor(d),
+		    	G: Math.floor(d),
+		    	B: Math.floor(d)
+		    };
+}
---
+++ jsc/jquery.application.js
@@ -0,0 +1,108 @@
+/*
+* Version: 00.01
+* Date: 29.10.2011
+* By: usw-usf.de
+*/
+
+$(function () {
+
+    // tell us the required event...
+    var _clickEventType = ((document.ontouchstart !== null) ? 'click' : 'touchstart');
+
+    // determine whether in web clip mode or not
+    if (("standalone" in window.navigator) && !window.navigator.standalone) {
+        if (navigator.userAgent.match(/iPhone/i))
+            // make the font-size smaller cause of the limited window size
+            $("html").css("font-size", "12.8px");
+        
+        $("#pleasenote").show().fadeOut(15000);
+    }
+
+    // bind to the color magic
+    $("#hexpad li").bind(_clickEventType, function () {
+        var $this = $(this),
+            character = $this.html(),
+            $hex = $("#value", "#output");
+
+        //come on it's only HEX
+        if ($hex.text().length >= 6)
+            return;
+
+        $hex.html($hex.html() + character);
+
+        if ($hex.text().length >= 3) {
+            var rgbVal = hexToRgb($hex.text());
+
+            if (rgbVal != null) {
+                $("#rgb-value", "#output").html(rgbVal.R + " " + rgbVal.G + " " + rgbVal.B);
+
+                var hslVal = rgbToHsl(rgbVal.R, rgbVal.G, rgbVal.B);
+                $("#hsl-value", "#output").html(hslVal.H + "<b>°</b> " + hslVal.S + "<b>%</b> " + hslVal.L + "<b>%</b>");
+
+                var fontColor = setFontColor(rgbVal);
+                $("#preview", "#output").css({ "background-color": "#" + $hex.text(), "color": "rgba(" + fontColor.R + "," + fontColor.G + "," + fontColor.B + "," + "1)" });
+            }
+        }
+    });
+
+    //online or offline detection
+    function checkNetworkStatus() {
+        if (navigator.onLine) {
+            // set up a call to a static JSON object stored on the server.
+            // the JSON object is not within our manifest. 
+            // got it, we're online. otherwise we're offline.
+            $.ajaxSetup({
+                async: true,
+                cache: false,
+                context: $("#status"),
+                dataType: "json",
+                error: function () {
+                    // to keep it simple: every error means offline for us
+                    showNetworkStatus(false);
+                },
+                success: function () {
+                    showNetworkStatus(true);
+                },
+                timeout: 5000,
+                type: "GET",
+                url: "jsc/network.js"
+            });
+            $.ajax();
+        } else {
+            showNetworkStatus(false);
+        }
+    }
+
+    // set the CSS class according to the network status
+    function showNetworkStatus(online) {
+        var $e = $("#online");
+        if (online) {
+            $e.addClass("online").removeClass("offline");
+        } else {
+            $e.addClass("offline").removeClass("online");
+        }
+    }
+
+    // a simple reset
+    function clear() {
+        $("#value, #rgb-value, #hsl-value", "#output").empty();
+        $("#preview", "#output").css({ "background-color": "", "color": "#000" });
+    }
+
+    //mobile specific events
+    window.ondevicemotion = function (event) {
+        var ax = event.accelerationIncludingGravity.x;
+        if (ax > 10.0) clear();
+    };
+
+    // bind the clear functionality to our button
+    $("#preview").bind(_clickEventType, function () {
+        clear();
+    });
+
+    // bind the network status check
+    $(document.body).bind("online", checkNetworkStatus);
+    $(document.body).bind("offline", checkNetworkStatus);
+    checkNetworkStatus();
+
+});
